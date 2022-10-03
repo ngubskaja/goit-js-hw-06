dfec94eb74ca3270d1d434427911931b93a4a1a6
Gubska Nastya <97690198+ngubskaja@users.noreply.github.com>
2022-10-03 17:55:59 +0300

    add

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,18 @@
 const itemCategoriesEl = document.querySelector(`#categories`);
-
+console.log(itemCategoriesEl);
 
 const liOfCategoriesEl = itemCategoriesEl.querySelectorAll(`li.item`);
+
 console.log(`Number of categories:`, liOfCategoriesEl.length);
 
 const counter = parent => { return parent.childElementCount };
 const message = elements => {
     elements.forEach(element => {
         const firstElem = element.firstElementChild;
-        const secondElem = firstElem.nextElementSibling
+        // const secondElem = firstElem.nextElementSibling;
+        const lastElem = element.lastElementChild;
         console.log(`Category: ${firstElem.textContent}`);
-        console.log(`Elements: ${counter(secondElem)}`);
+        console.log(`Elements: ${counter(lastElem )}`);
     });
 };
 
